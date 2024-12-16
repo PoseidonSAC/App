2e2a0b04029aff55c622435e3db8efa3f5863756
JosephRamirez-F4fu <U20211C828@upc.edu.pe>
2024-12-16 09:03:20 -0500

    Agregar configuración de timeout y encabezados de aceptación en la instancia de axios

--- src/core/api/index.ts
+++ src/core/api/index.ts
@@ -4,7 +4,9 @@ import { ENV } from "../constant/env";
 export const api = axios.create({
   baseURL: ENV.API_URL,
   withCredentials: true,
+  timeout: 5000,
   headers: {
     "Content-Type": "application/json",
+    Accept: "application/json",
   },
 });
